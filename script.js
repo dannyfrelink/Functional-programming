da438432dc18598260addb80b6b043797176b671
dannyfrelink <danny-frelink@hotmail.com>
2021-10-27 10:57:20 +0200

    poging 1 functional programming

--- script.js
+++ script.js
@@ -4,13 +4,22 @@ const lodash = require('lodash');
 const futureJob = 'Wat wil je worden als je groot bent?';
 
 // Antwoorden van vraag ophalen
-const answersFutureJob = data.map(answers =>
-    answers[futureJob]
-);
+// const answersFutureJob = data.map(answers =>
+//     answers[futureJob]
+// );
 
-// Eerste letter van woord hoofdletter geven
-const capitalizeJob = data.map(word => 
-    lodash.capitalize(word[futureJob])
-);
+// // Eerste letter van woord hoofdletter geven
+// const capitalizeJob = data.map(word => 
+//     lodash.capitalize(word[futureJob])
+// );
 
-console.log(capitalizeJob);
+// console.log(capitalizeJob);
+
+
+function getAnswers(question) {
+    data.map(answers =>
+        answers[question]
+    );
+}
+
+console.log(getAnswers('Wat wil je worden als je groot bent?'));
